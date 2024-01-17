6cd8a63adccd3b1d4d861a70c1a90daad694099d
Herghys <helmi.santosa1@gmail.com>
2024-01-17 09:35:16 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Minimal API Learn",
+        Description = "Making The API",
+        Version = "v1",
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API Learn V1");
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
